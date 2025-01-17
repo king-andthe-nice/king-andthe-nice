2b1465cc3d5ad105e267a20c05ad78a5f91f37f3
king-andthe-nice <3356369582@qq.com>
2025-01-17 20:10:19 +0800

    3

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
+import './index.css'
 
 const app = createApp(App)
-
+//
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
