8ea9ae9b90cdc9a63da4b8f82e45f30ab09f231f
king-andthe-nice <3356369582@qq.com>
2025-01-18 02:48:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || ''
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  base: '/king-andthe-nice/',  // 直接使用仓库名称
   server: {
     port: 3000
   }
