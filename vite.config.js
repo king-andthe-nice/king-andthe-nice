d3de359b36325453346b733d09cb2ebd63c70b2f
king-andthe-nice <3356369582@qq.com>
2025-01-18 00:41:20 +0800

    6

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 获取仓库名称
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || ''
+
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   server: {
     port: 3000
   }
